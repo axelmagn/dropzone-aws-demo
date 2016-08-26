9ca76f508a47adeacc371cbe1840c94789b71bc9
Axel Magnuson <axelmagn@gmail.com>
2016-08-25 18:04:12 -0600

    Landing page (#14)
    * write beginnings of an upload page
    
    * Add Dropzone widget on uploads page

--- dzdemo/dzcore/views.py
+++ dzdemo/dzcore/views.py
@@ -1,6 +1,13 @@
 # from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.shortcuts import redirect
 
 
 class IndexView(TemplateView):
-    template_name = "index.html"
+    template_name = "upload.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super(TemplateView, self).dispatch(request, *args, **kwargs)
+        else:
+            return redirect('/login/')
